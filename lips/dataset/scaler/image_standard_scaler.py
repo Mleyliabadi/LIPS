1f798d5a7ab3c0239393957b62620a1d80a3f3d7
David Danan <david.danan@irt-systemx.fr>
2022-05-02 10:39:16 +0000

    Generalize scaler for any dimension for channel

--- lips/dataset/scaler/image_standard_scaler.py
+++ lips/dataset/scaler/image_standard_scaler.py
@@ -8,23 +8,31 @@ import numpy as np
 # from . import Scaler
 # from . import StandardScaler
 
+
+def put_along_axis_per_channel(channel,channel_index,channel_data,overall_data):
+    i = [slice(None)]*channel_data.ndim
+    i[channel_index] = channel
+    overall_data[tuple(i)] = channel_data
+    return overall_data
+
+
 class StandardScalerPerChannel():
     """Standard scaler per channel
 
     On each channel, apply
     X - mean(X) / std(X)
     """
-    def __init__(self,channelIndex_x=1,channelIndex_y=1):
-        self.channelIndex_x = channelIndex_x
-        self.channelIndex_y = channelIndex_y
+    def __init__(self,channel_index_x=1,channel_index_y=1):
+        self.channel_index_x = channel_index_x
+        self.channel_index_y = channel_index_y
         self._m_x = []
         self._m_y = []
         self._std_x = []
         self._std_y = []
 
     def fit(self, x, y):
-        for channel in range(x.shape[self.channelIndex_x]):
-            x_channel_axis=x.take(channel,axis=self.channelIndex_x)    
+        for channel in range(x.shape[self.channel_index_x]):
+            x_channel_axis=x.take(channel,axis=self.channel_index_x)    
             x_channel=x_channel_axis.reshape(x.shape[0], -1)
             m_x_channel= np.mean(x_channel, axis=0)
             self._m_x.append(m_x_channel)
@@ -32,8 +40,8 @@ class StandardScalerPerChannel():
             std_x_channel[np.abs(std_x_channel) <= 1e-1] = 1
             self._std_x.append(std_x_channel)
 
-        for channel in range(y.shape[self.channelIndex_y]):    
-            y_channel_axis=y.take(channel,axis=self.channelIndex_y)
+        for channel in range(y.shape[self.channel_index_y]):    
+            y_channel_axis=y.take(channel,axis=self.channel_index_y)
             y_channel=y_channel_axis.reshape(y.shape[0], -1)
             m_y_channel= np.mean(y_channel, axis=0)
             self._m_y.append(m_y_channel)
@@ -42,23 +50,31 @@ class StandardScalerPerChannel():
             self._std_y.append(std_y_channel)
 
     def transform(self, x, y):
-        shapeBychannel_x=x.shape[:self.channelIndex_x]+x.shape[self.channelIndex_x+1:]
+        shapeBychannel_x=x.shape[:self.channel_index_x]+x.shape[self.channel_index_x+1:]
         transformed_x=np.zeros_like(x)
-        for channel in range(x.shape[self.channelIndex_x]):
-            x_channel_axis=x.take(channel,axis=self.channelIndex_x)        
+        for channel in range(x.shape[self.channel_index_x]):
+            x_channel_axis=x.take(channel,axis=self.channel_index_x)        
             x_channel=x_channel_axis.reshape(x_channel_axis.shape[0], -1)
             x_channel-= self._m_x[channel]
             x_channel/= self._std_x[channel]
-            transformed_x[:,channel,::]=x_channel.reshape(shapeBychannel_x)
+            x_channel_reshaped=x_channel.reshape(shapeBychannel_x)
+            transformed_x=put_along_axis_per_channel(channel=channel,
+                                                     channel_index=self.channel_index_x,
+                                                     channel_data=x_channel_reshaped,
+                                                     overall_data=transformed_x)
 
-        shapeBychannel_y=y.shape[:self.channelIndex_y]+y.shape[self.channelIndex_y+1:]
+        shapeBychannel_y=y.shape[:self.channel_index_y]+y.shape[self.channel_index_y+1:]
         transformed_y=np.zeros_like(y)
-        for channel in range(y.shape[self.channelIndex_y]):
-            y_channel_axis=y.take(channel,axis=self.channelIndex_y)        
+        for channel in range(y.shape[self.channel_index_y]):
+            y_channel_axis=y.take(channel,axis=self.channel_index_y)        
             y_channel=y_channel_axis.reshape(y_channel_axis.shape[0], -1)
             y_channel-= self._m_y[channel]
             y_channel/= self._std_y[channel]
-            transformed_y[:,channel,::]=y_channel.reshape(shapeBychannel_y)
+            y_channel_reshaped=y_channel.reshape(shapeBychannel_y)
+            transformed_y=put_along_axis_per_channel(channel=channel,
+                                                     channel_index=self.channel_index_y,
+                                                     channel_data=y_channel_reshaped,
+                                                     overall_data=transformed_y)
 
         return transformed_x, transformed_y
 
@@ -68,14 +84,18 @@ class StandardScalerPerChannel():
         return transformed_x, transformed_x
 
     def inverse_transform(self, y):
-        shapeBychannel_y=y.shape[:self.channelIndex_y]+y.shape[self.channelIndex_y+1:]
+        shapeBychannel_y=y.shape[:self.channel_index_y]+y.shape[self.channel_index_y+1:]
         transformed_y=np.zeros_like(y)
-        for channel in range(y.shape[self.channelIndex_y]):
-            y_channel_axis=y.take(channel,axis=self.channelIndex_y)        
+        for channel in range(y.shape[self.channel_index_y]):
+            y_channel_axis=y.take(channel,axis=self.channel_index_y)        
             y_channel=y_channel_axis.reshape(y_channel_axis.shape[0], -1)
             y_channel*= self._std_y[channel]
             y_channel+= self._m_y[channel]
-            transformed_y[:,channel,::]=y_channel.reshape(shapeBychannel_y)
+            y_channel_reshaped=y_channel.reshape(shapeBychannel_y)
+            transformed_y=put_along_axis_per_channel(channel=channel,
+                                                     channel_index=self.channel_index_y,
+                                                     channel_data=y_channel_reshaped,
+                                                     overall_data=transformed_y)
         return transformed_y
 
 
