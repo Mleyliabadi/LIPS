e3d086d61e1d16d028f7af375ffedbe9bb56a4bf
David Danan <david.danan@irt-systemx.fr>
2022-05-09 15:23:59 +0000

    First superficial version of wheel benchmark

---
+++ lips/evaluation/transport_evaluation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def TransportEvaluation():
+    return None
+
+class TransportEvaluation():
+    def __init__(self):
+    	self.toto="truc"
+
+    def from_benchmark(self,benchmark):
+    	return None
