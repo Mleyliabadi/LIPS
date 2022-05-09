220b9a7d4b731487ac3f4b38f1ae023c3da00a4c
David Danan <david.danan@irt-systemx.fr>
2022-05-09 14:53:41 +0000

    Add u_net model

---
+++ lips/augmented_simulators/torch_models/u_net.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Torch CNN U-net model
+"""
+import pathlib
+from typing import Union
+import json
+
+import numpy as np
+
+from ...dataset import DataSet
+from ...dataset.scaler import Scaler
+from ...logger import CustomLogger
+from ...config import ConfigManager
+from ...utils import NpEncoder
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+#Credits goes to https://github.com/milesial/Pytorch-UNet
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class TorchUnet(nn.Module):
+    def __init__(self,
+                 name: Union[str, None]=None,
+                 scaler: Union[Scaler, None]=None,
+                 bench_config_path: Union[str, pathlib.Path, None]=None,
+                 bench_config_name: Union[str, None]=None,
+                 sim_config_path: Union[str, None]=None,
+                 sim_config_name: Union[str, None]=None,
+                 log_path: Union[None, str]=None,
+                 **kwargs):
+        super().__init__()#name=name, log_path=log_path, **kwargs)
+
+        # Benchmark configurations
+        self.bench_config = ConfigManager(section_name=bench_config_name, path=bench_config_path)
+        # The config file associoated to this model
+        sim_config_name = sim_config_name if sim_config_name is not None else "DEFAULT"
+        sim_config_path_default = pathlib.Path(__file__).parent.parent / "configurations" / "torch_fc.ini"
+        sim_config_path = sim_config_path if sim_config_path is not None else sim_config_path_default
+        self.sim_config = ConfigManager(section_name=sim_config_name, path=sim_config_path)
+        self.name = name if name is not None else self.sim_config.get_option("name")
+        # scaler
+        self.scaler = scaler
+        # Logger
+        self.log_path = log_path
+        self.logger = CustomLogger(__class__.__name__, log_path).logger
+
+        self.bilinear = True
+        self.n_channels = None if kwargs.get("input_size") is None else kwargs["input_size"]
+        self.n_classes = None if kwargs.get("output_size") is None else kwargs["output_size"]
+
+
+    def build_model(self):
+        """Build the model flow
+        """
+        self.inc = DoubleConv(self.n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if self.bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, self.n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+    def process_dataset(self, dataset: DataSet, training: bool):
+        """process the datasets for training and evaluation
+
+        This function transforms all the dataset into something that can be used by the neural network (for example)
+
+        Parameters
+        ----------
+        dataset : DataSet
+            _description_
+        scaler : Scaler, optional
+            _description_, by default True
+        training : bool, optional
+            _description_, by default False
+
+        Returns
+        -------
+        DataLoader
+            _description_
+        """
+        if training:
+            self._infer_size(dataset)
+            batch_size = self.params["train_batch_size"]
+            extract_x, extract_y = dataset.extract_data()
+            if self.scaler is not None:
+                extract_x, extract_y = self.scaler.fit_transform(extract_x, extract_y)
+        else:
+            batch_size = self.params["eval_batch_size"]
+            extract_x, extract_y = dataset.extract_data()
+            if self.scaler is not None:
+                extract_x, extract_y = self.scaler.transform(extract_x, extract_y)
+
+        torch_dataset = TensorDataset(torch.from_numpy(extract_x).float(), torch.from_numpy(extract_y).float())
+        data_loader = DataLoader(torch_dataset, batch_size=batch_size, shuffle=self.params["shuffle"])
+        return data_loader
+
+    def _post_process(self, data):
+        if self.scaler is not None:
+            processed = self.scaler.inverse_transform(data)
+        return processed
+
+    def _infer_size(self, dataset: DataSet):
+        """Infer the size of the model
+
+        Parameters
+        ----------
+        dataset : DataSet
+            _description_
+
+        Returns
+        -------
+        None
+            _description_
+        """
+        *dim_inputs, self.output_size = dataset.get_sizes()
+        self.input_size = np.sum(dim_inputs)
+
+    def get_metadata(self):
+        res_json = {}
+        res_json["input_size"] = self.input_size
+        res_json["output_size"] = self.output_size
+        return res_json
+
+    def _save_metadata(self, path: str):
+        #super()._save_metadata(path)
+        #if self.scaler is not None:
+        #    self.scaler.save(path)
+        res_json = {}
+        res_json["input_size"] = self.input_size
+        res_json["output_size"] = self.output_size
+        with open((path / "metadata.json"), "w", encoding="utf-8") as f:
+            json.dump(obj=res_json, fp=f, indent=4, sort_keys=True, cls=NpEncoder)
+
+    def _load_metadata(self, path: str):
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+        #super()._load_metadata(path)
+        #if self.scaler is not None:
+        #    self.scaler.load(path)
+        with open((path / "metadata.json"), "r", encoding="utf-8") as f:
+            res_json = json.load(fp=f)
+        self.input_size = res_json["input_size"]
+        self.output_size = res_json["output_size"]
