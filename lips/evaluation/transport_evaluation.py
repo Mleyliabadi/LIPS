4dd936609b9e1da13efd42cc62587fbc53dc2f6f
David Danan <david.danan@irt-systemx.fr>
2022-05-16 12:11:43 +0000

    Build complete database to test Benchmark logic

--- lips/evaluation/transport_evaluation.py
+++ lips/evaluation/transport_evaluation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def TransportEvaluation():
-    return None
+# def TransportEvaluation():
+#     return None
 
 class TransportEvaluation():
     def __init__(self):
