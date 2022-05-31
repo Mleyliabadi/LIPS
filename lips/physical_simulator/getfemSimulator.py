9839d0131105a4fe5a2bef2123e4e459bc09b07b
David Danan <david.danan@irt-systemx.fr>
2022-05-31 14:51:31 +0000

    Test several parameter value before running full augmented chain

--- lips/physical_simulator/getfemSimulator.py
+++ lips/physical_simulator/getfemSimulator.py
@@ -50,8 +50,45 @@ class GetfemSimulator(PhysicalSimulator):
     def __str__(self):
         return str(self._simulator)
 
+import math
+
+
+def check_static_benchmark():
+    physicalDomain={
+        "Mesher":"Getfem",
+        "refNumByRegion":{"HOLE_BOUND": 1,"CONTACT_BOUND": 2, "EXTERIOR_BOUND": 3},
+        "wheelDimensions":(8.0,15.0),#(13.5,18.0),
+        "meshSize":1.5
+    }
+
+
+    physicalProperties={
+        "ProblemType":"StaticMechanicalStandard",
+        "materials":[["ALL", {"law":"LinearElasticity","young":5.98e6,"poisson":0.495} ]],#[["ALL", {"law":"IncompressibleMooneyRivlin", "MooneyRivlinC1": 1, "MooneyRivlinC2":1} ]],
+        #"neumann":[["HOLE_BOUND",{"type" : "StandardNeumann", "fx":0.0, "fy":-1e2}] ],
+        "neumann":[["HOLE_BOUND", {"type": "RimRigidityNeumann", "Force": 1.0e7}]],
+        "incompressibility":True,
+        "contact":[ ["CONTACT_BOUND",{"type" : "Plane","gap":0.0,"fricCoeff":0.5}] ]
+    }
+
+
+    # physicalProperties={
+    #     "ProblemType":"StaticMechanicalStandard",
+    #     "materials":[["ALL", {"law":"SaintVenantKirchhoff","young":5.98e6,"poisson":0.495} ]],
+    #     "incompressibility":True,
+    #     "sources":[["ALL",{"type" : "Uniform","source_x":0.0,"source_y":0}] ],
+    #     #"neumann":[["HOLE_BOUND",{"type" : "StandardNeumann", "fx":0.0, "fy":-5}] ],
+    #     #"neumann":[["HOLE_BOUND",{"type" : "RimRigidityNeumann", "Force":5}] ],
+    #     "dirichlet":[["HOLE_BOUND",{"type" : "scalar", "Disp_Amplitude":3, "Disp_Angle":-math.pi/2}] ],
+    #     "contact":[ ["CONTACT_BOUND",{"type" : "Plane","gap":0.0,"fricCoeff":0.0}] ]
+    # }
+    mySimulator=GetfemSimulator(physicalDomain=physicalDomain,physicalProperties=physicalProperties)
+    mySimulator.build_model()
+    mySimulator.run_problem()
+    mySimulator._simulator.ExportSolutionInGmsh(filename="StaticBenchmark.pos")
+
+
 def check_static():
-    import math
     physicalDomain={
         "Mesher":"Getfem",
         "refNumByRegion":{"HOLE_BOUND": 1,"CONTACT_BOUND": 2, "EXTERIOR_BOUND": 3},
@@ -104,4 +141,5 @@ def check_quasi_static_rolling():
 
 if __name__ == '__main__':
     #check_static()
-    check_quasi_static_rolling()
+    #check_quasi_static_rolling()
+    check_static_benchmark()
