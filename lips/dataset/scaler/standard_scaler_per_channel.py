7a7c038e5c25e06f97686f02b799672d72827ba6
David Danan <david.danan@irt-systemx.fr>
2022-06-01 16:01:44 +0000

    Add save/load method in scaler per channel

--- lips/dataset/scaler/standard_scaler_per_channel.py
+++ lips/dataset/scaler/standard_scaler_per_channel.py
@@ -4,8 +4,11 @@ The Scaler class offering the normalization capabilities
 from typing import Union
 import pathlib
 import numpy as np
+import json
 
 from lips.dataset.scaler.scaler import Scaler
+from lips.utils import NpEncoder
+
 #from . import Scaler
 
 def put_along_axis_per_channel(channel,channel_index,channel_data,overall_data):
@@ -97,12 +100,38 @@ class StandardScalerPerChannel():
                                                      overall_data=transformed_y)
         return transformed_y
 
+    def save(self, path: Union[str, pathlib.Path]):
+        res_json = {}
+        res_json["_m_x"] = self._m_x
+        res_json["_m_y"] = self._m_y
+        res_json["_std_x"] = self._std_x
+        res_json["_std_y"] = self._std_y
+
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+        if not path.exists():
+            path.mkdir(parents=True)
+        with open((path / "scaler_params.json"), "w", encoding="utf-8") as f:
+            json.dump(obj=res_json, fp=f, indent=4, sort_keys=True, cls=NpEncoder)
+
+    def load(self, path: Union[str, pathlib.Path]):
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+        with open((path / "scaler_params.json"), "r", encoding="utf-8") as f:
+            res_json = json.load(fp=f)
+
+        self._m_x = np.array(res_json["_m_x"], dtype=np.float32)
+        self._m_y = np.array(res_json["_m_y"], dtype=np.float32)
+        self._std_x = np.array(res_json["_std_x"], dtype=np.float32)
+        self._std_y = np.array(res_json["_std_y"], dtype=np.float32)
+
 
 if __name__ =="__main__":
     np.random.seed(42)
     featuresRandom=100*np.random.rand(10,4,2,2)
     labelsRandom=2000*np.random.rand(10,8,2,2)
 
+    #Check scaler reversibility
     myScaler=StandardScalerPerChannel()
     transformed_x, transformed_y=myScaler.fit_transform(x=featuresRandom, y=labelsRandom)
     assert transformed_x.shape==featuresRandom.shape
@@ -110,10 +139,35 @@ if __name__ =="__main__":
 
     myOtherScaler=StandardScalerPerChannel()
     double_transformed_x,double_transformed_y=myOtherScaler.fit_transform(x=transformed_x, y=transformed_y)
-    np.testing.assert_almost_equal(transformed_x,double_transformed_x)
-    np.testing.assert_almost_equal(transformed_y,double_transformed_y)
+    np.testing.assert_almost_equal(actual=transformed_x,desired=double_transformed_x)
+    np.testing.assert_almost_equal(actual=transformed_y,desired=double_transformed_y)
 
     myInverseCheckScaler=StandardScalerPerChannel()
     transformed_x, transformed_y=myInverseCheckScaler.fit_transform(x=featuresRandom, y=labelsRandom)
     inverse_transformed_y=myInverseCheckScaler.inverse_transform(y=transformed_y)
     np.testing.assert_almost_equal(labelsRandom,inverse_transformed_y)
+
+    #CheckSaveLoad
+    myScaler_to_save=StandardScalerPerChannel()
+    transformed_x, transformed_y=myScaler_to_save.fit_transform(x=featuresRandom, y=labelsRandom)
+    m_x_original = myScaler_to_save._m_x
+    m_y_original = myScaler_to_save._m_y
+    std_x_original = myScaler_to_save._std_x
+    std_y_original = myScaler_to_save._std_y
+    myScaler_to_save.save(path=".")
+
+    myScaler_to_load = StandardScalerPerChannel()
+    myScaler_to_load.load(path=".")
+
+    m_x_load = myScaler_to_load._m_x
+    m_y_load = myScaler_to_load._m_y
+    std_x_load = myScaler_to_load._std_x
+    std_y_load = myScaler_to_load._std_y
+
+    np.testing.assert_almost_equal(actual=m_x_original,desired=m_x_load,decimal=4)
+    np.testing.assert_almost_equal(actual=m_y_original,desired=m_y_load,decimal=4)
+    np.testing.assert_almost_equal(actual=std_x_original,desired=std_x_load,decimal=4)
+    np.testing.assert_almost_equal(actual=std_y_original,desired=std_y_load,decimal=4)
+
+
+
