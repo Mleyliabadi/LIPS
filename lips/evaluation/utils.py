f0766c38de3f0050b15831cbc89e858c252417f1
mleyliabadi <mleyliabadi@gmail.com>
2022-07-07 00:47:13 +0200

    MetricFactory is added in utils replacing Mapper class

--- lips/evaluation/utils.py
+++ lips/evaluation/utils.py
@@ -1,23 +1,74 @@
-"""
-Usage:
-    Helper classes and functions for evaluation
+# Copyright (c) 2021, IRT SystemX (https://www.irt-systemx.fr/en/)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of LIPS, LIPS is a python platform for power networks benchmarking
 
-License:
-    Copyright (c) 2021, IRT SystemX (https://www.irt-systemx.fr/en/)
-    See AUTHORS.txt
-    This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
-    If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
-    you can obtain one at http://mozilla.org/MPL/2.0/.
-    SPDX-License-Identifier: MPL-2.0
-
-Description:
-    This file is part of LIPS, LIPS is a python platform for power networks benchmarking
-"""
 from typing import Callable
-import copy
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
+# import copy
+# from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_absolute_error
+from lips.metrics import DEFAULT_METRICS
 
+
+class MetricFactory:
+    """Factory method to register the metrics
+    """
+    def __init__(self):
+        self._creators = {}
+
+    def register_metric(self, metric_name: str, creator: Callable):
+        """Register new metrics
+
+        Parameters
+        ----------
+        metric_name : str
+            _description_
+        creator : Callable
+            _description_
+        """
+        self._creators[metric_name] = creator
+
+    def register_metric_dict(self, metric_dict: dict):
+        """register a dictionary including metrics
+
+        Parameters
+        ----------
+        metric_dict : dict
+            _description_
+        """
+        for key_, fun_ in metric_dict.items():
+            self.register_metric(key_, fun_)
+
+    def get_metric(self, metric_name:str):
+        """Get the required metric
+
+        Parameters
+        ----------
+        metric_name : str
+            _description_
+
+        Returns
+        -------
+        Callable
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
+        creator = self._creators.get(metric_name)
+        if not creator:
+            raise ValueError(metric_name)
+        return creator
+
+metric_factory = MetricFactory()
+metric_factory.register_metric_dict(DEFAULT_METRICS)
+
+'''
 class Mapper(object):
     """Mapper class
     This class create a mapping dict between evaluation criteria and their respective functions available in metrics module
@@ -106,4 +157,4 @@ class Mapper(object):
             new key
         """
         self.criteria[k_new] = self.criteria[k_old]
-
+'''
